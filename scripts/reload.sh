d6a76684c8939031806e154c4cab18cb34786831
Ziad Alzarka <ziadalzarka@gmail.com>
2021-01-23 16:55:45 +0200

    save pm2 on start

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -1,14 +1,12 @@
 source /home/ec2-user/.bash_profile
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "sn-aws-bootcamb-BE-Live-GN" ]
-then
-   echo 'executes the steps to deploy on Live-instances'
+if [ "$DEPLOYMENT_GROUP_NAME" == "sn-aws-bootcamb-BE-Live-GN" ]; then
+    echo 'executes the steps to deploy on Live-instances'
     # rm /folders/configs/sn-aws-bootcamb.json
     # cd /folders/configs
     yes | cp /folders/exe/sn-aws-bootcamb/envConfig/live/sn-aws-bootcamp.json /folders/configs/
-elif [ "$DEPLOYMENT_GROUP_NAME" == "sn-aws-bootcamb-BE-Dev-GN" ]
-then
-   echo 'executes the steps to deploy on Dev-instances'
+elif [ "$DEPLOYMENT_GROUP_NAME" == "sn-aws-bootcamb-BE-Dev-GN" ]; then
+    echo 'executes the steps to deploy on Dev-instances'
     # rm /folders/configs/coatconnect-user.json
     yes | cp /folders/exe/sn-aws-bootcamb/envConfig/dev/sn-aws-bootcamp.json /folders/configs/
 else
@@ -21,12 +19,13 @@ fi
 pm2 describe sn-aws-bootcamp-api
 RUNNING=$?
 
-if [ "${RUNNING}" -ne 0 ]; 
-then
+if [ "${RUNNING}" -ne 0 ]; then
     echo 'service not exist'
     pm2 start /folders/exe/sn-aws-bootcamb/index.js --name sn-aws-bootcamp-api
+    pm2 save
+    pm2 startup
 else
     echo 'service exist and starting to reload it '
     pm2 restart sn-aws-bootcamp-api
-fi;
+fi
 # pm2 restart user-api
